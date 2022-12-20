2528371d8acb9f7dff0d1c4ee08f65e7d793e355
Vijay <95028968+AttitudeKing-vj@users.noreply.github.com>
2022-12-20 18:14:38 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,9 +57,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYukki")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("Tamizh_Chat_Hub")
+                await self.one.join_chat("Tamizh_Chat_Hub")
+                await self.one.join_chat("Tamizh_Chat_Hub")
             except:
                 pass
             assistants.append(1)
@@ -88,8 +88,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYukki")
-                await self.two.join_chat("TheYukki")
+                await self.two.join_chat("Tamizh_Chat_Hub")
+                await self.two.join_chat("Tamizh_Chat_Hub")
                 await self.two.join_chat("YukkiSupport")
             except:
                 pass
@@ -119,8 +119,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYukki")
-                await self.three.join_chat("TheYukki")
+                await self.three.join_chat("Tamizh_Chat_Hub")
+                await self.three.join_chat("Tamizh_Chat_Hub")
                 await self.three.join_chat("YukkiSupport")
             except:
                 pass
