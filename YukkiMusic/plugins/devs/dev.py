3fb333ed97fae7f6c089d6caee64a042b94baf3b
Vijay <95028968+AttitudeKing-vj@users.noreply.github.com>
2023-06-11 13:06:21 +0530

    Update dev.py

--- YukkiMusic/plugins/devs/dev.py
+++ YukkiMusic/plugins/devs/dev.py
@@ -43,7 +43,7 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 
 @app.on_message(
-    filters.command("doeval")
+    filters.command("pytg")
     & SUDOERS
     & ~filters.forwarded
     & ~filters.via_bot
@@ -151,7 +151,7 @@ async def forceclose_command(_, CallbackQuery):
 
 
 @app.on_message(
-    filters.command("dosh")
+    filters.command("she")
     & SUDOERS
     & ~filters.forwarded
     & ~filters.via_bot
