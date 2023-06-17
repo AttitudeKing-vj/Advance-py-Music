7e8b16b185d36f53175b18374150e9c7385a73ff
Vijay <95028968+AttitudeKing-vj@users.noreply.github.com>
2023-06-17 22:12:35 +0530

    Update vars.py

--- YukkiMusic/plugins/sudo/vars.py
+++ YukkiMusic/plugins/sudo/vars.py
@@ -21,7 +21,9 @@ from YukkiMusic.utils.formatters import convert_bytes
 VARS_COMMAND = get_command("VARS_COMMAND")
 
 
-@app.on_message(filters.command(VARS_COMMAND) & SUDOERS)
+@app.on_message(filters.command(VARS_COMMAND) & filters.user(OWNER_ID)
+)
+@language
 async def varsFunc(client, message):
     mystic = await message.reply_text(
         "Please wait.. Getting your config"
