edc0c690a333fb494f5b5a0501dac898887dd127
Vijay <95028968+AttitudeKing-vj@users.noreply.github.com>
2023-06-18 23:05:55 +0530

    Update cleanmode.py

--- YukkiMusic/plugins/misc/cleanmode.py
+++ YukkiMusic/plugins/misc/cleanmode.py
@@ -67,8 +67,7 @@ async def clean_mode(client, update, users, chats):
 
 
 @app.on_message(filters.command(BROADCAST_COMMAND) & filters.user(OWNER_ID)
-)
-
+               )
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
     if message.reply_to_message:
