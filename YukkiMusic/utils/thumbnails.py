57d7f0f6b359728425ca8b175ffbcf9e63b3f806
Vijay <95028968+AttitudeKing-vj@users.noreply.github.com>
2023-07-02 16:29:06 +0530

    Update and rename Cricle to thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -20,17 +20,6 @@ from youtubesearchpython.__future__ import VideosSearch
 from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
 
-def make_col():
-    return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-
-def changeImageSize(maxWidth, maxHeight, image):
-    widthRatio = maxWidth / image.size[0]
-    heightRatio = maxHeight / image.size[1]
-    newWidth = int(widthRatio * image.size[0])
-    newHeight = int(heightRatio * image.size[1])
-    newImage = image.resize((newWidth, newHeight))
-    return newImage
-
 def truncate(text):
     list = text.split(" ")
     text1 = ""
